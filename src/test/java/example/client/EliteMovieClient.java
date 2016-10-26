047e3eb95e56f613ad33d280038e6de174ba106e
Alejandro Perdomo <aperdomobo@gmail.com>
2016-10-26 17:05:18 -0500

    fix indentation

--- src/test/java/example/client/EliteMovieClient.java
+++ src/test/java/example/client/EliteMovieClient.java
@@ -6,17 +6,17 @@ import example.dto.ShowtimeDto;
 import example.request.EliteMovieRequest;
 
 public class EliteMovieClient {
-  
+
   private EliteMovieRequest request;
-  
+
   public EliteMovieClient() {
     this.request = new EliteMovieRequest();
   }
-  
+
   public MovieDto[] getMovies() {
     return this.request.getMovies();
   }
-  
+
   public int reserve(SeatParameterDto[] seats) {
     return this.request.reserve(3, seats);
   }
